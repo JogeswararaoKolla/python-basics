b8a0f0c0cf182021d5d27c37fd5ce5ce120e30d5
Jogeswararao Kolla <jogeskolla@gmail.com>
2021-07-17 22:08:45 -0700

    roll dice functional way

--- roll_dice.py
+++ roll_dice.py
@@ -1,10 +1,18 @@
 import random
+
+
+def roll_dice():
+    dice_total = 0
+    for i in range(2):
+        roll = random.randint(1, 6)
+        dice_total = dice_total + roll
+        print("Roll Dice #", i, roll)
+    return dice_total
+
+
 guess = int(input('Guess the dice roll:\n'))
-rolldice1 = random.randint(1, 6)
-rolldice2 = random.randint(1, 6)
-print("Computer rolled Dice1 " + str(rolldice1))
-print("Computer rolled Dice2 " + str(rolldice2))
-if guess == (rolldice1 + rolldice2):
+result = roll_dice()
+if guess == result:
     print("You win")
 else:
-    print("You Loose Computer dice roll is " + str(rolldice1 + rolldice2))
+    print("You Loose Computer dice roll is ", result)
